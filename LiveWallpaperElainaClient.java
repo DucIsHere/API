f3caaa8fdb2190be98d7cd24d1d2a2792a80d7ec
DucIsHere <thelegendwolf07@gmail.com>
2025-08-11 21:14:26 +0700

    Create LiveWallpaperElainaClient.java

---
+++ LiveWallpaperElainaClient.java
@@ -0,0 +1,10 @@
+package com.ducishere.livewallpaperelaina.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LiveWallpaperElainaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Init mod
+    }
+}
